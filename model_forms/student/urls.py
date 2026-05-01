a8fc35314fa3ea2a267f518d2bee92b1b5ed4313
Prasun99 <prasunkhadkasp@gmail.com>
2026-05-02 05:20:20 +0545

    second commit in CRUD operation

--- model_forms/student/urls.py
+++ model_forms/student/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
   path('',views.student_create,name="student_create"),
+  path('list/',views.student_list,name="student_list"),
+  path('detail/<int:pk>/',views.student_detail,name="student_detail"),
 ]
--- model_forms/student/views.py
+++ model_forms/student/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from . forms import studentForm
+from . models import student
 def student_create(request):
   form = studentForm()
 
@@ -9,3 +10,9 @@ def student_create(request):
       form.save()
       return render(request,'success.html')
   return render(request,'student.html',{'form':form})
+def student_list(request):
+  students = student.objects.all()
+  return render(request , 'list.html',{'students':students})
+def student_detail(request,pk):
+  sstudent=get_object_or_404(student,pk=pk)
+  return render(request , 'detail.html',{'sstudent':sstudent})
